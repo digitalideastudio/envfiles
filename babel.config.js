032845df274bdf41056fb175336cbee30b1fcb73
Serhii Matrunchyk <serhii@digitalidea.studio>
2020-12-09 20:39:24 +0200

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,5 @@
 module.exports = {
   presets: [
-    [
-      '@vue/app',
-      {
-        useBuiltIns: 'entry',
-      },
-    ],
-  ],
-  plugins: [
-    [
-      'transform-imports',
-      {
-        vuetify: {
-          transform: 'vuetify/es5/components/${member}', // eslint-disable-line
-          preventFullImport: true,
-        },
-      },
-    ],
-    '@babel/plugin-proposal-export-default-from',
-    ['@babel/plugin-proposal-class-properties', { spec: true }],
+    '@vue/cli-plugin-babel/preset',
   ],
 };
